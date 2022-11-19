c18a99005c39ddb6bdfb88efdf0e013f8d98f885
amanu251 <amanmenge251@gmail.com>
2022-11-19 02:56:22 +0000

    all in one

---
+++ get_helpers.c
@@ -0,0 +1,109 @@
+#include "shell.h"
+
+/**
+ * get_help - function that retrieves help messages according builtin
+ * @datash: data structure (args and input)
+ * Return: Return 0
+*/
+int get_help(data_shell *datash)
+{
+
+	if (datash->args[1] == 0)
+		aux_help_general();
+	else if (_strcmp(datash->args[1], "setenv") == 0)
+		aux_help_setenv();
+	else if (_strcmp(datash->args[1], "env") == 0)
+		aux_help_env();
+	else if (_strcmp(datash->args[1], "unsetenv") == 0)
+		aux_help_unsetenv();
+	else if (_strcmp(datash->args[1], "help") == 0)
+		aux_help();
+	else if (_strcmp(datash->args[1], "exit") == 0)
+		aux_help_exit();
+	else if (_strcmp(datash->args[1], "cd") == 0)
+		aux_help_cd();
+	else if (_strcmp(datash->args[1], "alias") == 0)
+		aux_help_alias();
+	else
+		write(STDERR_FILENO, datash->args[0],
+		      _strlen(datash->args[0]));
+
+	datash->status = 0;
+	return (1);
+}
+
+/**
+ * get_error - calls the error according the builtin, syntax or permission
+ * @datash: data structure that contains arguments
+ * @eval: error value
+ * Return: error
+ */
+int get_error(data_shell *datash, int eval)
+{
+	char *error;
+
+	switch (eval)
+	{
+	case -1:
+		error = error_env(datash);
+		break;
+	case 126:
+		error = error_path_126(datash);
+		break;
+	case 127:
+		error = error_not_found(datash);
+		break;
+	case 2:
+		if (_strcmp("exit", datash->args[0]) == 0)
+			error = error_exit_shell(datash);
+		else if (_strcmp("cd", datash->args[0]) == 0)
+			error = error_get_cd(datash);
+		break;
+	}
+
+	if (error)
+	{
+		write(STDERR_FILENO, error, _strlen(error));
+		free(error);
+	}
+
+	datash->status = eval;
+	return (eval);
+}
+
+/**
+ * get_builtin - builtin that pais the command in the arg
+ * @cmd: command
+ * Return: function pointer of the builtin command
+ */
+int (*get_builtin(char *cmd))(data_shell *)
+{
+	builtin_t builtin[] = {
+		{ "env", _env },
+		{ "exit", exit_shell },
+		{ "setenv", _setenv },
+		{ "unsetenv", _unsetenv },
+		{ "cd", cd_shell },
+		{ "help", get_help },
+		{ NULL, NULL }
+	};
+	int i;
+
+	for (i = 0; builtin[i].name; i++)
+	{
+		if (_strcmp(builtin[i].name, cmd) == 0)
+			break;
+	}
+
+	return (builtin[i].f);
+}
+
+/**
+ * get_sigint - Handle the crtl + c call in prompt
+ * @sig: Signal handler
+ */
+void get_sigint(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n^-^ ", 5);
+}
